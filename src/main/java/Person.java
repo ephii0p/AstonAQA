a8f9a53fd1f93a0f01a204ddc6b8d56910339639
ephii0p <belunkind@gmail.com>
2024-10-12 17:49:48 +0400

    Lession5

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -15,10 +15,9 @@ public class Person {
         this.age = age;
     }
 
-
     public void info() {
 
-        System.out.println("ФИО:" + FIO + " " + "Должность: " + position + " "+ "email: " + email + " " +
+        System.out.println("ФИО:" + FIO + " " + "Должность: " + position + " " + "email: " + email + " " +
                 "Телефон: " + phoneNumber + " " + "Зарплата: " + payment + " " + "Возвраст: " + age);
     }
 
@@ -26,8 +25,9 @@ public class Person {
         Person Person = new Person("Иванов Иван Иванович", "Менеджер", "test@mail.ru", "+7123456789", 150000, 42);
         Person.info();
     }
-    public static void worker(){
-        Person [] persArray = new Person[5];
+
+    public static void worker() {
+        Person[] persArray = new Person[5];
         persArray[0] = new Person("Петров Петр Петрович", "Тестировщик", "test2@mail.ru", "+7123456788", 123123, 32);
         persArray[1] = new Person("Петров Алексей Алексеевич", "Лид", "test3@mail.ru", "+7123456777", 123123, 34);
         persArray[2] = new Person("Петров Андрей Андреевич", "Сеньор", "test4@mail.ru", "+7123456666", 123123, 312);
@@ -35,7 +35,4 @@ public class Person {
         persArray[4] = new Person("Петров Федор Федорвич", "Начальник", "test6@mail.ru", "+712344444", 123123, 23);
 
     }
-
-
-
 }
